fc3beb973dd2ea559a7957426327b6bff331e2d2
Ossi Siipola <ossi.siipola@helsinki.fi>
2018-11-19 18:38:59 +0200

    creating cucumber template to creating new article tip

---
+++ src/test/java/rbtips/Stepdefs.java
@@ -0,0 +1,27 @@
+package rbtips;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import static org.junit.Assert.*;
+
+public class Stepdefs {
+    
+    
+    
+    @Given("^new tip command is given$")
+    public void new_tip_command_is_given() throws Throwable {
+        
+    }
+
+    @When("^a valid headline \"([^\"]*)\" and valid author \"([^\"]*)\" and valid url \"([^\"]*)\"$")
+    public void a_valid_headline_and_valid_author_and_valid_url(String headline, String author, String url) throws Throwable {
+        
+    }
+
+    @Then("^artile, which headline is \"([^\"]*)\" and author is \"([^\"]*)\" and url is \"([^\"]*)\" is found$")
+    public void artile_which_headline_is_and_author_is_and_url_is_is_found(String headline, String author, String url) throws Throwable {
+        
+    }
+  
+}
