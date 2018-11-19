8bbe792cc752c53cf526f8f28b5a23c08bc5a9aa
Markus Kuosmanen <markus.kuosmanen@helsinki.fi>
2018-11-19 18:30:52 +0200

    small edit

---
+++ src/main/java/rbtips/ui/Main.java
@@ -0,0 +1,19 @@
+package rbtips.ui;
+
+import java.util.Scanner;
+import rbtips.dao.ArticleDao;
+import rbtips.dao.Database;
+import rbtips.domain.AppService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Database db = new Database("jdbc:sqlite:tips.db");
+        ArticleDao dao = new ArticleDao(db);
+        AppService app = new AppService(dao);
+        //ylläoleva app täytyy injektoida käyttöliittymäluokalle metodien kutsumista varten
+        UserInterface ui = new UserInterface(scanner, app);
+        ui.start();
+    }
+}
