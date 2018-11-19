8aeee42f120d513f81b3c1cb829c88e4b70b6395
Anssi Lintulampi <anssi.lintulampi@helsinki.fi>
2018-11-19 16:27:36 +0200

    database constructor in Main

--- src/main/java/rbtips/domain/Main.java
+++ src/main/java/rbtips/domain/Main.java
@@ -1,12 +1,15 @@
 package rbtips.domain;
 
 import java.util.Scanner;
+import rbtips.dao.Database;
 import rbtips.ui.UserInterface;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Database db = new Database("/database/tips.db");
+        //tietokanta pitää injektoida UI:hin konstruktorin parametrina!
         UserInterface ui = new UserInterface(scanner);
         ui.start();
     }
