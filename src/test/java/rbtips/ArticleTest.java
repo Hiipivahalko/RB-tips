3c410ce351c34d203571609e5620519002149366
PyryV <pyry.vekkeli@helsinki.fi>
2018-11-19 22:27:03 +0200

    ArticleTest done

--- src/test/java/rbtips/ArticleTest.java
+++ src/test/java/rbtips/ArticleTest.java
@@ -1,22 +1,44 @@
+
 package rbtips;
 
-
-import org.junit.*;
-import static org.junit.Assert.assertEquals;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 import rbtips.domain.Article;
+
+
 
 public class ArticleTest{
     Article article;
 
     @Before
     public void SetUp(){
-        article = new Article("Otsikko", "Kirjoittaja", "www.123.com");
+        article = new Article("Headline", "Author", "www.article.com");
         
     }
 
     @Test
     public void rightHeadline(){
-        assertEquals("Otsikko" ,article.getHeadline());
+        assertEquals("Headline" ,article.getHeadline());
+    }
+    
+    @Test
+    public void rightAuthor(){
+        assertEquals("Author", article.getAuthor());
+    }
+    
+    @Test
+    public void rightUrl(){
+        assertEquals("www.article.com", article.getUrl());
+    }
+    
+    @Test
+    public void rightToString(){
+        assertEquals("Author: Headline url: www.article.com", article.toString());
     }
     
 }
+
