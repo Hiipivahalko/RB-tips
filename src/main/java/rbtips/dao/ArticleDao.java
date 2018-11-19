ffd95653112ae36b4a4f6cded2d1f1dab205c78b
Anssi Lintulampi <anssi.lintulampi@helsinki.fi>
2018-11-19 16:21:02 +0200

    article dao updated

--- src/main/java/rbtips/dao/ArticleDao.java
+++ src/main/java/rbtips/dao/ArticleDao.java
@@ -1,19 +1,23 @@
-
 package rbtips.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import rbtips.domain.Article;
 
-
 public class ArticleDao implements ArticleDaoApi {
-    
+
     private Database db;
 
     @Override
     public void create(Article article) throws SQLException {
         Connection conn = db.getConnection();
-        //luodaan yhteys tietokantaan, tehdään query ja lisätään TODO
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO Artikkelit(otsikko, kirjoittaja) VALUES (?, ?)");
+        stmt.setString(0, article.getHeadline());
+        stmt.setString(1, article.getAuthor());
+        stmt.executeUpdate();
+        stmt.close();
+        conn.close();
     }
-    
+
 }
