c13a2cb7e2d3fb02567566a710852c5640c25bf1
Olli Rousu <olli.rousu@hotmail.com>
2018-11-19 17:16:56 +0200

    hae artikkelit

--- src/main/java/rbtips/dao/ArticleDao.java
+++ src/main/java/rbtips/dao/ArticleDao.java
@@ -2,7 +2,9 @@ package rbtips.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import rbtips.domain.Article;
 
 public class ArticleDao implements ArticleDaoApi {
@@ -18,6 +20,29 @@ public class ArticleDao implements ArticleDaoApi {
         stmt.executeUpdate();
         stmt.close();
         conn.close();
+    }
+
+    @Override
+    public ArrayList<Article> getAll() throws SQLException {
+        ArrayList<Article> articles = new ArrayList<>();
+        
+        Connection conn = db.getConnection();
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM Artikkelit");
+
+        ResultSet rs = stmt.executeQuery();
+
+        while (rs.next()) {
+            System.out.println("täällä ollaaan");
+            Article article = new Article(rs.getString("otsikko"), rs.getString("kirjoittaja"));
+            System.out.println(article.getAuthor());
+            articles.add(article);
+        }
+        
+        stmt.close();
+        rs.close();
+        conn.close();
+        
+        return articles;
     }
 
 }
--- src/main/java/rbtips/dao/ArticleDaoApi.java
+++ src/main/java/rbtips/dao/ArticleDaoApi.java
@@ -2,11 +2,12 @@
 package rbtips.dao;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import rbtips.domain.*;
 
 
 public interface ArticleDaoApi {
     
     void create (Article article) throws SQLException;
-    
+    ArrayList<Article> getAll() throws SQLException;
 }
