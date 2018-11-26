7cb0d69b5e8560eff198226d2dfdb2d993b13c19
Markus Kuosmanen <markus.kuosmanen@helsinki.fi>
2018-11-27 00:23:47 +0200

    all tests passing

--- src/test/java/rbtips/domain/AppServiceTest.java
+++ src/test/java/rbtips/domain/AppServiceTest.java
@@ -1,6 +1,9 @@
 package rbtips.domain;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -8,11 +11,12 @@ import rbtips.dao.*;
 
 public class AppServiceTest {
 
+    Database db;
     AppService app;
 
     @Before
-    public void setUp() {
-        Database db = new Database("jdbc:sqlite:test.db");
+    public void setUp() throws Exception {
+        db = new Database("jdbc:sqlite:test.db");
         ArticleDao articleDao = new ArticleDao(db, "Articles");
         TagDao tagDao = new TagDao(db, "Tag");
         app = new AppService(articleDao, tagDao);
@@ -30,6 +34,7 @@ public class AppServiceTest {
         saveArticle();
         saveArticle();
         saveArticle();
+
         ArrayList<Article> articles = app.getAllArticles();
         assertEquals(3, articles.size());
     }
@@ -52,4 +57,15 @@ public class AppServiceTest {
         app.saveArticle("Breaking news", "Journalist", "http://news.com", "news");
     }
 
+    @After
+    public void tearDown() throws Exception {
+        Connection conn = db.getConnection();
+        PreparedStatement stmt1 = conn.prepareStatement("DROP TABLE Articles");
+        PreparedStatement stmt2 = conn.prepareStatement("DROP TABLE Tag");
+        stmt1.execute();
+        stmt2.execute();
+        stmt1.close();
+        stmt2.close();
+        conn.close();
+    }
 }
