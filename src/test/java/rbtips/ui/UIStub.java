4749c954e55843c9116abc16a3c85fc2368d0714
Anssi Lintulampi <anssi.lintulampi@helsinki.fi>
2018-11-26 17:58:37 +0200

    tests added for searchHeadline method in ArticleDaoTest

--- src/test/java/rbtips/ui/UIStub.java
+++ src/test/java/rbtips/ui/UIStub.java
@@ -37,5 +37,10 @@ public class UIStub implements UI {
     public void showTips() {
         
     }
+
+    @Override
+    public void search() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
