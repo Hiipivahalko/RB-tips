b74284c37978a9e47393cd3810f3d94b50c6aa32
Mikko Leinonen <mikko.leinonen91@gmail.com>
2018-11-27 19:15:06 +0200

    adding new tip now happens through popup-window, better scaling when adjusting the size of the program screen

---
+++ src/main/java/rbtips/ui/AddNewTipSceneController.java
@@ -0,0 +1,60 @@
+package rbtips.ui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import rbtips.domain.*;
+
+public class AddNewTipSceneController implements Initializable {
+    
+    private Stage stage;
+    private Main application;
+    private AppService appService;
+    
+    @FXML Label messageLabel;
+    @FXML private TextField headline; 
+    @FXML private TextField author;
+    @FXML private TextField url;
+    
+
+    public void setApplication(Main application) {
+        this.application = application;
+    }
+    
+    public void setAppService(AppService appService) {
+        this.appService = appService;
+    }
+    
+    public void display(String title, String message, Parent root1) {
+        stage = new Stage();
+        stage.setTitle(title);
+        messageLabel.setText(message);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(new Scene(root1));
+        stage.showAndWait();
+    }
+    
+    @FXML
+    private void handleAddNewTipButton(ActionEvent event) throws SQLException, IOException {
+        Article article = new Article(headline.getText(), author.getText(), url.getText());
+        if (appService.saveArticle(headline.getText(), author.getText(), url.getText())) {
+            stage.close();
+        }
+        
+    }
+ 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+    
+}
--- src/main/java/rbtips/ui/MainPageSceneController.java
+++ src/main/java/rbtips/ui/MainPageSceneController.java
@@ -1,6 +1,7 @@
 
 package rbtips.ui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
@@ -8,7 +9,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import rbtips.domain.AppService;
@@ -20,22 +23,9 @@ public class MainPageSceneController {
     private Main application;
     private AppService appService;
     private ObservableList<Article> articleList;
+    private AddNewTipSceneController addNew;
     
-    @FXML
-    private TextField headline; 
-    
-    @FXML private TextField author;
-    
-    @FXML private TextField url;
-    
-    
-    @FXML
-    private TableView<Article> tableView;
-    
-
-    /**
-     * Initializes the controller class.
-     */
+    @FXML private TableView<Article> tableView;
     
     public void setApplication(Main application) {
         this.application = application;
@@ -51,17 +41,20 @@ public class MainPageSceneController {
     }
     
     @FXML
-    private void handleAddNewTip(ActionEvent event) throws SQLException {
-        String headLine = headline.getText();
-        String a = author.getText();
-        String URL = url.getText();
-        
-        Article article = new Article(headLine, a, URL);
-        if (appService.saveArticle(headLine, a, URL)) {
-            setArticles();
-        }
-        
+    private void openAddNewTipScene(ActionEvent event) throws SQLException, IOException {
+        initializeAddNewTipScene();
+        setArticles();
     }
+    
+    public void initializeAddNewTipScene() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/AddNewTipScene.fxml"));
+        Parent root1 = (Parent) loader.load();
+        addNew = loader.getController();
+        addNew.setApplication(application);
+        addNew.setAppService(appService);
+        addNew.display("Add new tip!", "Input information", root1);
+    }
+    
     
 //    @Override
 //    public void initialize(URL url, ResourceBundle rb) {
