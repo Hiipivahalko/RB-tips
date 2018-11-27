244b94d1fc4907544f3ed8e27fbc47ddfbd8e3b9
PyryV <pyry.vekkeli@helsinki.fi>
2018-11-27 18:27:14 +0200

    TagDaoTest and AppServiceTest fixed

--- src/test/java/rbtips/dao/TagDaoTest.java
+++ src/test/java/rbtips/dao/TagDaoTest.java
@@ -13,17 +13,12 @@ public class TagDaoTest {
 
     Database database;
     TagDao tagDao;
-    ArticleTagDao articleTagDao;
 
     @Before
     public void setUp() {
         
-<<<<<<< HEAD
         database = new Database("jdbc:sqlite:test.db");
-        articleTagDao = new ArticleTagDao(database, "ArticleTag");
-        tagDao = new TagDao(database, articleTagDao, "Tags");
-=======
->>>>>>> 6903e297325cd6dc4424ca9367251aeb7955daa1
+        tagDao = new TagDao(database, "Tags");
     }
 
 
