d1e28a2c76194b9bf1109647dbfcdf991a117f90
Anssi Lintulampi <anssi.lintulampi@helsinki.fi>
2018-11-30 16:06:47 +0200

    filter by headline works in gui

--- src/main/java/rbtips/ui/MainPageSceneController.java
+++ src/main/java/rbtips/ui/MainPageSceneController.java
@@ -2,15 +2,13 @@
 package rbtips.ui;
 
 import java.io.IOException;
-import java.net.URL;
 import java.sql.SQLException;
-import java.util.ResourceBundle;
+import java.util.ArrayList;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -28,6 +26,7 @@ public class MainPageSceneController {
     private ViewTipSceneController viewTip;
     
     @FXML private TableView<Article> tableView;
+    @FXML private TextField filter;
     
     public void setApplication(Main application) {
         this.application = application;
@@ -64,6 +63,13 @@ public class MainPageSceneController {
             Article a = tableView.getSelectionModel().getSelectedItem();
             setViewTipScene(a);
         }
+    }
+    
+    @FXML
+    private void handleFilterButton(ActionEvent event) throws IOException {
+        String filterCondition = filter.getText();
+        ObservableList<Article> filteredArticleList = FXCollections.observableArrayList(appService.searchHeadline(filterCondition));
+        tableView.setItems(filteredArticleList);
     }
     
     public void setViewTipScene(Article a) throws IOException {
