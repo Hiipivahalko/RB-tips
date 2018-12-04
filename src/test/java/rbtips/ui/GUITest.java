1055f61b0d92dc2649e67cf8795d6fcbf9d4a034
PyryV <pyry.vekkeli@helsinki.fi>
2018-12-04 15:37:42 +0200

    GUITest created

---
+++ src/test/java/rbtips/ui/GUITest.java
@@ -0,0 +1,21 @@
+package rbtips.ui;
+
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class GUITest {
+    
+    public GUITest() {
+    }
+    
+    
+    
+    
+}
