102fa6a8c84ea7275989fb1da26ec498f3045970
Markus Kuosmanen <markus.kuosmanen@helsinki.fi>
2018-12-04 18:22:06 +0200

    TagDaoTests done

--- src/test/java/rbtips/dao/TagDaoTest.java
+++ src/test/java/rbtips/dao/TagDaoTest.java
@@ -4,25 +4,25 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TagDaoTest {
 
     Database database;
+    ArticleDao articleDao;
     TagDao tagDao;
 
     @Before
     public void setUp() {
         database = new Database("jdbc:sqlite:test.db");
-        tagDao = new TagDao(database, "Tags");
+        articleDao = new ArticleDao(database, "Articles");
+        tagDao = new TagDao(database, "Tag");
         database.initializeDatabase();
     }
 
-    /*@Test
+    @Test
     public void addingOneTagWorks() throws SQLException {
         String[] tags = new String[]{"tag"};
         tagDao.addTagsIfNotAlreadyExist(tags);
@@ -35,14 +35,27 @@ public class TagDaoTest {
         tagDao.addTagsIfNotAlreadyExist(tags);
         assertTrue(tagDao.alreadyExists("tag1"));
         assertTrue(tagDao.alreadyExists("tag2"));
-    }*/
+    }
+
+    @Test
+    public void findIdByNameReturnsOneIdInArrayList() throws SQLException {
+        String[] tags = new String[]{"tag"};
+        tagDao.addTagsIfNotAlreadyExist(tags);
+        assertTrue(tagDao.findIdByName(tags).size() == 1);
+    }
+
+    @Test
+    public void findByNameReturnTwoIdsInArrayList() throws SQLException {
+        String[] tags = new String[]{"tag", "tag2"};
+        tagDao.addTagsIfNotAlreadyExist(tags);
+        assertTrue(tagDao.findIdByName(tags).size() == 2);
+    }
 
     @After
-    public void tearDown() throws Exception {
-        Connection connection = database.getConnection();
-        PreparedStatement statement = connection.prepareStatement("DROP TABLE Articles");
-        statement.execute();
-        statement.close();
-        connection.close();
+    public void tearDown() throws SQLException {
+        try (Connection connection = database.getConnection();
+                PreparedStatement statement = connection.prepareStatement("DROP TABLE Articles")) {
+            statement.execute();
+        }
     }
 }
