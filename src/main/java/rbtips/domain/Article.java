075fd0b5e3a9859b1906f89704e9e54d0f2d32d5
Markus Kuosmanen <markus.kuosmanen@helsinki.fi>
2018-12-10 16:01:10 +0200

    work work work

--- src/main/java/rbtips/domain/Article.java
+++ src/main/java/rbtips/domain/Article.java
@@ -5,10 +5,12 @@ import java.util.Objects;
 public class Article extends Tip {
 
     private final String url;
+    private String date;
 
     public Article(String headline, String author, String url) {
         super(headline, author, "");
         this.url = url;
+        this.date = null;
     }
 
     public Article(String headline, String author, String url, String tags) {
@@ -50,4 +52,7 @@ public class Article extends Tip {
     public void setTags(String tags) {
         super.setTags(tags);
     }*/
+    public void setTimeStamp(String timeStamp) {
+        this.date = timeStamp;
+    }
 }
